5c8756a3fb270f7e9b51908d4ed8a640d9280e6b
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-08-02 15:42:27 +0900

    Add sign-up function

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'weights/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "weights#index"
+  resources :users, only: [:index,:edit, :update]
 end
