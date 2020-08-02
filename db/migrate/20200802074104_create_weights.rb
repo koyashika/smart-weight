d53d1251206beda428f808464c5169ff643c6358
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-08-02 16:48:40 +0900

    Create weight model

---
+++ db/migrate/20200802074104_create_weights.rb
@@ -0,0 +1,9 @@
+class CreateWeights < ActiveRecord::Migration[6.0]
+  def change
+    create_table :weights do |t|
+      t.integer :kg
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
