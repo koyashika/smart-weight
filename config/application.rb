8e3f0384ac814c8a35d6fba624198bced0224992
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-08-04 18:36:24 +0900

    Change timezome Tokyo

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module SmartWeight
     config.generators do |g|
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
       g.stylesheets false
       g.javascripts false
       g.helper false
