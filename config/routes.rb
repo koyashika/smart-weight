d263a9f2301f982540dbb220cdadafeafd0b083b
koyashika <u_wh.sunnnny.xx@softbank.ne.jp>
2020-08-05 19:24:58 +0900

    Implementation of weight transmission function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "weights#index"
-  resources :users, only: [:index,:edit, :update]
+  resources :users, only: [:index, :edit, :update]
+  resources :weights, only: [:index,:new, :create]
+  post '/' => 'weights#create'
 end
